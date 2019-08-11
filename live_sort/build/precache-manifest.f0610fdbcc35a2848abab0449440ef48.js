d3aa1ba881dda23c87086979982483fde6d8bec3
Feebon1235 <mirilianazin@gmail.com>
2019-08-12 01:12:58 +0300

    v.0.4: configured build

---
+++ live_sort/build/precache-manifest.f0610fdbcc35a2848abab0449440ef48.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da5c080f235c18294beaa2f2872fe79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f81439eef8b217bfbe5",
+    "url": "/static/css/main.5d0c3b09.chunk.css"
+  },
+  {
+    "revision": "5d24d0508a04450241c0",
+    "url": "/static/js/2.20e33e00.chunk.js"
+  },
+  {
+    "revision": "3f81439eef8b217bfbe5",
+    "url": "/static/js/main.94bb1f26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8813b6d1c117ca9c0cb292a49f5309b",
+    "url": "/static/media/background_engage.c8813b6d.mp3"
+  },
+  {
+    "revision": "3db3b71f3a129d55d2a7bdacc5ba525b",
+    "url": "/static/media/background_fire.3db3b71f.mp3"
+  },
+  {
+    "revision": "d09e3db6da3dd3488d42602c698c44a1",
+    "url": "/static/media/background_fluid.d09e3db6.mp3"
+  },
+  {
+    "revision": "1bb0ceefeb0f9745e02e675d93b4ef64",
+    "url": "/static/media/background_light.1bb0ceef.mp3"
+  },
+  {
+    "revision": "e3fcf07a9d3879a8c8fc112650956486",
+    "url": "/static/media/background_person.e3fcf07a.mp3"
+  },
+  {
+    "revision": "d57d2364c687b9ecd9f0b8882e3480fe",
+    "url": "/static/media/logo.d57d2364.png"
+  },
+  {
+    "revision": "6f776815b35247152d9bca9624f8a399",
+    "url": "/static/media/view_array.6f776815.jpg"
+  },
+  {
+    "revision": "3929532cc4a5aff6107717a284e63f07",
+    "url": "/static/media/view_bubble.3929532c.jpg"
+  },
+  {
+    "revision": "7b795b6569ad251dec05af04901de7c3",
+    "url": "/static/media/view_column.7b795b65.jpg"
+  }
+]);
--- live_sort/src/components/View/Thumbnail/Thumbnail.js
+++ live_sort/src/components/View/Thumbnail/Thumbnail.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './Thumbnail.css'
-
 /**
  * Thumbnail of {component} for demonstrate and selecting
  * **/
